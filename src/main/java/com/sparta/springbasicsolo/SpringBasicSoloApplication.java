c5639935560c9558c517908ecff03f64b38015fc
원지연 <dnjs229@gmail.com>
2024-05-13 14:57:16 +0900

    init

---
+++ src/main/java/com/sparta/springbasicsolo/SpringBasicSoloApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springbasicsolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicSoloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicSoloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springbasicsolo/SpringBasicSoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springbasicsolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicSoloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
